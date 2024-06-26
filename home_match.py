bce7e2d38e9aece63370fb722db34ea861247b37
Robyn F H Veitch <oddert@gmail.com>
2024-06-26 17:25:54 +0100

    with much pain added a lancedb connection

--- home_match.py
+++ home_match.py
@@ -7,6 +7,8 @@ from langchain.llms import OpenAI
 import lancedb
 import tiktoken
 import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
 
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, Request
@@ -19,20 +21,52 @@ from config import TABLE_NAME, LISTINGS_RAW_FILENAME
 
 from routes.home import router as home
 
+# tokeniser = tiktoken.get_encoding('cl100k_base')
+tokeniser = AutoTokenizer.from_pretrained('gpt2')
+tokeniser.add_special_tokens({'pad_token': '[PAD]'})
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+	print('[load db]')
 	# load db
 	df = pd.read_csv(f'./{LISTINGS_RAW_FILENAME}')
 	# tokeniser = tiktoken.get_encoding('cl100k_base')
-	tokeniser = AutoTokenizer.from_pretrained('distilbert-base-uncased')
-	df['vector'] = [tokeniser.encode(x) for x in df['description']]
-	print(df)
+
+	# Ensure all vectors are lists of integers - thank you ChatGPT...
+	df['vector'] = [list(map(int, tokeniser.encode(x, padding='max_length'))) for x in df['description']]
+	print('[df defined]')
 	db = lancedb.connect('./lancedb')
-	db.create_table(TABLE_NAME, df, exist_ok=True, on_bad_vectors='fill')
+
+	try:
+		db.drop_table(TABLE_NAME)
+	except:
+		print('table doesn\'t exist')
+
+	db.create_table(TABLE_NAME, df, exist_ok=True, on_bad_vectors='drop')
+	print('[table created]')
 	table = db.open_table(TABLE_NAME)
-	print(table.search('bike lanes').limit(1))
+	print(table.search(tokeniser.encode('bike', padding='max_length')).limit(1))
+	print('[finished]')
 	yield
+	# db.close()
 	# close db connection
+
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+# 	# load db
+# 	df = pd.read_csv(f'./{LISTINGS_RAW_FILENAME}')
+# 	tokeniser = tiktoken.get_encoding('cl100k_base')
+# 	# tokeniser = AutoTokenizer.from_pretrained('distilbert-base-uncased')
+# 	df['vector'] = [tokeniser.encode(x) for x in df['description']]
+# 	print(df)
+# 	db = lancedb.connect('./lancedb')
+# 	# db.drop_table(TABLE_NAME, )
+# 	db.create_table(TABLE_NAME, df, exist_ok=True, on_bad_vectors='fill')
+# 	table = db.open_table(TABLE_NAME)
+# 	print(table.search(tokeniser.encode('bike lanes')).limit(1))
+# 	yield
+# 	db.close()
+# 	# close db connection
 
 app = FastAPI(lifespan=lifespan)
 
@@ -47,6 +81,14 @@ def one(request: Request):
 			'request': request,
 		},
 	)
+
+@app.get('/s', response_class=HTMLResponse)
+def one(request: Request):
+	db = lancedb.connect('./lancedb')
+	table = db.open_table(TABLE_NAME)
+	# query = table.search('bike lanes').limit(1).to_list()
+	query = table.search(tokeniser.encode('bike lanes', padding='max_length')).limit(1).to_list()
+	return str(query)
 
 app.include_router(home, tags=['home'])
 
